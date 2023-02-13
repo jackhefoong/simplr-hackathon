2fa8fccd45018543d30cad15a8dd0af4c4977492
Jackhe <jackhefoong@gmail.com>
2023-02-13 16:19:41 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-// import {useState} from 'react';
-import {Routes, Route} from 'react-router-dom';
-// import jwt_decode from 'jwt-decode';
-// import TopNav from './components/partials/Navbar';
-// import Register from './components/forms/Register';
-// import Login from './components/forms/Login';
-import Home from './components/Home';
-// import DatingCards from './components/DatingCards';
-import './components/style/App.css';
-// import Swal from 'sweetalert2';
+import TopNav from './components/partials/Navbar';
+import "materialize-css/dist/css/materialize.min.css";
+
+
 
 function App() {
-  // const [token, setToken] = useState('');
-  // const [userData, setUserData] = useState({});
-
-  // let handleLogin = (data) => {
-  //   let decoded = jwt_decode(data);
-  //   setToken(data);
-  //   setUserData(decoded);
-
-  //   localStorage.setItem('userData', JSON.stringify(decoded));
-  //   localStorage.setItem('token', data);
-  // };
-
-  // let handleLogout = () => {
-  //   setToken();
-  //   setUserData({});
-  //   localStorage.removeItem('token');
-  //   localStorage.removeItem('userData');
-  // };
-
   return (
     <div className="App">
-      {/* <TopNav handleLogout={handleLogout} /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/match" element={<Match />} />
-        <Route path="/register" element={<Register handleLogin={handleLogin} />} />
-        <Route path="/login" element={<Login handleLogin={handleLogin} />} />
-        <Route path="/myprofile" element={<DatingProfile />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/reports" element={<Reports />} /> */}
-      </Routes>
+      <TopNav />
+      <h1>Hello World!</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  <App />,
+  document.getElementById('root')
 );
-
