8dd6d26192ad6027d9fb02c4887c6e8a2cb3e339
Jackhe <jackhefoong@gmail.com>
2023-02-13 19:34:01 +0800

    sign in

--- src/components/sidebar/components/Content.js
+++ src/components/sidebar/components/Content.js
@@ -3,14 +3,12 @@ import { Box, Flex, Stack } from "@chakra-ui/react";
 //   Custom components
 import Brand from "components/sidebar/components/Brand";
 import Links from "components/sidebar/components/Links";
-import SidebarCard from "components/sidebar/components/SidebarCard";
 import React from "react";
 
 // FUNCTIONS
 
 function SidebarContent(props) {
   const { routes } = props;
-  // SIDEBAR
   return (
     <Flex direction='column' height='100%' pt='25px' borderRadius='30px'>
       <Brand />
@@ -19,15 +17,6 @@ function SidebarContent(props) {
           <Links routes={routes} />
         </Box>
       </Stack>
-
-      <Box
-        ps='20px'
-        pe={{ md: "16px", "2xl": "0px" }}
-        mt='60px'
-        mb='40px'
-        borderRadius='30px'>
-        <SidebarCard />
-      </Box>
     </Flex>
   );
 }
